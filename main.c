d1957aba549090321bd40ee46cc53a7246e01f4b
Mauzerov <mauzer.official.1@gmail.com>
2025-01-18 13:19:33 +0100

    remove logic.c

--- main.c
+++ main.c
@@ -13,6 +13,42 @@
 
 #include <SDL2/SDL.h>
 
+int save_rand(Game * game) {
+    return game->seed = rand();
+}
+
+bool _random_position(Game * game, Point * entity) {
+    bool valid = false;
+    if (game->snake_size > GAME_SIZE * GAME_SIZE)
+        return valid;
+    do {
+        valid = true;
+        entity->x = save_rand(game) % GAME_SIZE;
+        entity->y = save_rand(game) % GAME_SIZE;
+
+        for (int i = 0; i < game->snake_size; i++) {
+            if (is_overlapping(&(game->snake[i]), entity)) {
+                valid = false;
+                break;
+            }
+        }
+
+        if (is_overlapping(&game->berry, entity)
+        ||  is_overlapping(&game->apple, entity)) {
+            continue;
+        }
+
+        for (int i = 0; i < PORTER_COUNT * 2; i++) {
+            if (is_overlapping(entity, &game->porters[i])) {
+                valid = false;
+                break;
+            }
+        }
+    } while (!valid);
+    return valid;
+}
+
+
 void update_animations(Game * game) {
     static int animation_frame = 0;
     game->apple.animation_frame++;
@@ -185,8 +221,6 @@ int main() {
     SDL_Renderer * renderer = NULL;
     
     Game game = (Game) {
-        .apple = (Entity) { 0 },
-        .berry = (Entity) { 0 },
         .apple_actions = { apple_action_shorten, apple_action_slowdown },
     };
     new_game(&game);
